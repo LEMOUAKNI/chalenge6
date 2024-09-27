9cc0e58e8dca721e73f90f58ff3d6d870cb4d466
LEMOUAKNI <lemhamza7@gmail.com>
2024-09-27 14:49:25 +0100

    gg

---
+++ chalenge 6/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+
+    printf("Entrer la valeur de a : ");
+    scanf("%d", &a);
+
+    printf("Entrer la valeur de b : ");
+    scanf("%d", &b);
+
+    int somme = a + b;
+    int difference = a - b;
+    int produit = a * b;
+
+
+    printf("a + b = %d\n", somme);
+    printf("a - b = %d\n", difference);
+    printf("a * b = %d\n", produit);
+
+
+
+    return 0;
+}
